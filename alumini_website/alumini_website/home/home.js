83c0b4ad74dd29c0c5c4aced3219cd2dc4173902
Manoj Kumar v <118556854+manukotari@users.noreply.github.com>
2024-10-09 18:53:31 +0530

    Add files via upload

---
+++ alumini_website/alumini_website/home/home.js
@@ -0,0 +1,29 @@
+// gellary script
+document.addEventListener('DOMContentLoaded', function () {
+    var TrandingSlider = new Swiper('.tranding-slider', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        loop: true,  // Loop through the slides
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+        },
+        autoplay: {
+            delay: 2000, // Delay between transitions (in milliseconds)
+            disableOnInteraction: false, // Keep autoplay running even when user interacts
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+});
+
